817510c4b10588ce71c28a139a8fa6890bcc8676
Khaloud <hamedkhaloud71@gmail.com>
2023-12-05 13:11:06 +0400

    Add project files.

---
+++ Week4-ReportClass/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Week4_ReportClass
+{
+    internal class Program
+    {
+
+        public int id { get; set; }
+        public string Gender { get; set; }
+        public string name { get; set; }
+        public int TotalSales { get; set; }
+
+        
+
+        static void Main(string[] args)
+        {
+          Program[] Pro = new Program[]
+          {
+            new Program {id = 101 ,name ="khaloud", Gender="Female" ,TotalSales =75000 },
+            new Program {id = 102 ,name ="Sara", Gender="Female" ,TotalSales =6000 },
+            new Program {id = 103 ,name ="Hamed", Gender="Male" ,TotalSales =30920 },
+            new Program {id = 104 ,name ="Reem", Gender="Female" ,TotalSales =11222 },
+            new Program {id = 105 ,name ="Ahamed", Gender="Male" ,TotalSales =1920 },
+            new Program {id = 106 ,name ="Johana", Gender="Female" ,TotalSales =11222 },
+            new Program {id = 107 ,name ="Maryam", Gender="Male" ,TotalSales =89202 },
+            new Program {id = 108 ,name ="Mona", Gender="Female" ,TotalSales =3000 },
+           };
+
+            Console.WriteLine("Employees sale count: \n");
+
+            Report report = new Report();
+            report.TotalSales(Pro);
+            report.TotalSalesMore30(Pro);
+            report.TotalSalesLess30(Pro);
+
+
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("------------------With delegate----------------------\n\n");
+
+            //dataType | functionName | (calssName + plase where value is save ) =>  calcolation 
+
+            bool greater60k(Program p) => p.TotalSales >= 60000;
+            bool between30k_60k(Program p) => p.TotalSales < 60000 && p.TotalSales >= 30000;
+            bool less30k(Program p) => p.TotalSales < 30000;
+
+            report.ProcesEmployee(Pro, "Report of Sale more than 60 k:", greater60k);
+            report.ProcesEmployee(Pro, "Report of Sale between 30k and 60k:", between30k_60k);
+            report.ProcesEmployee(Pro, "Report of Sale less than 30k:", less30k);
+
+
+            //report.ProcesEmployee(Pro, "Report of Sale less than 30k:",(program) => program.TotalSales < 30000);
+
+        }
+    }
+}
---
+++ Week4-ReportClass/Report.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Week4_ReportClass
+{
+    internal class Report
+    {  
+        //delegate example:                 
+
+        public delegate bool salesCondition(Program p);
+
+        //       functionName  --  className to serch from it --  function + filter condation
+        public void ProcesEmployee(Program[] pp ,string Title , salesCondition con)
+        {
+            Console.WriteLine(Title);
+            Console.WriteLine("----------------------------------------");
+            foreach (Program p in pp)
+            {
+                if (con(p))
+                {
+
+                    Console.WriteLine($"{p.id} | {p.name} | {p.Gender} | {p.TotalSales}");
+
+                }
+            }
+            Console.WriteLine("\n\n");
+
+        }
+
+
+
+
+        //-------------------------------------------------------------------------------------
+        //-----------------------------------with out delegate--------------------------------------------------
+        public Report() { }
+        public void TotalSales(Program[] prog)
+        {
+            Console.WriteLine("Report for emplyee total Sales more than 60 k:");
+            Console.WriteLine("----------------------------------------");
+            foreach (Program program in prog)
+            {
+                if (program.TotalSales >= 60000) {
+
+                    Console.WriteLine($"{program.id} | {program.name} | {program.Gender} | {program.TotalSales}");
+
+                }
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public void TotalSalesMore30(Program[] prog)
+        {
+            Console.WriteLine("Report for emplyee total Sales more than 30k and less than 60k :");
+            Console.WriteLine("----------------------------------------");
+            foreach (Program program in prog)
+            {
+                if (program.TotalSales < 60000 && program.TotalSales >=30000)
+                {
+
+                    Console.WriteLine($"{program.id} | {program.name} | {program.Gender} | {program.TotalSales}");
+
+                }
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public void TotalSalesLess30(Program[] prog)
+        {
+            Console.WriteLine("Report for emplyee total Sales less than 30k :");
+            Console.WriteLine("----------------------------------------");
+            foreach (Program program in prog)
+            {
+                if (program.TotalSales < 30000)
+                {
+
+                    Console.WriteLine($"{program.id} | {program.name} | {program.Gender} | {program.TotalSales}");
+
+                }
+            }
+            Console.WriteLine("\n\n");
+        }
+
+
+
+    }
+}
